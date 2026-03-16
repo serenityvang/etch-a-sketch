f00152db70296dbda2d6548f88ef1ea42c385f5a
Serenity Vang <serenvang23@gmail.com>
2026-03-16 11:10:20 -0500

    added a resize grid button to change how big or small the grid is max is 100

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
 const container = document.querySelector(".container");
-const button = document.querySelector(".gridSize");
+const button = document.getElementById("resizeBtn");
+
+const reset = document.getElementById("reset");
 
 // create the grid canvas
 function createGrid(size){
+    container.innerHTML = "";
+
+    const squareSize = 640 / size; // taking the size of the grid container and dividing it by the size
 
     for(let i = 0; i < size * size; i++){
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         square.style.opacity = "0.1";
         container.appendChild(square);
     }
-    
+
     // hovering over the squares changes the background color of the squares to black
     const squares = document.querySelectorAll(".square");
     // iterate through each square and add a mouse listener to "hover"
@@ -25,9 +32,25 @@ function createGrid(size){
                 square.style.opacity = (currentOpacity + 0.1).toString();
             }
         });
-        
+
     });
 
 }
 
 createGrid(16);
+
+button.addEventListener("click", () => {
+    let newSize = prompt("Enter the number of squares per side (Maximum 100):");
+    newSize = parseInt(newSize);
+
+    if(newSize && newSize > 0 && newSize <= 100){
+        createGrid(newSize);
+    } else {
+        alert("Please enter a valid number between 1 and 100");
+    }
+});
+
+// makes the graph back to a 16 x16;
+reset.addEventListener("click", () => {
+    createGrid(16);
+});
