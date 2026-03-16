378616ed97792768a76f17d288b0d0bafba0297b
Serenity Vang <serenvang23@gmail.com>
2026-03-16 11:34:35 -0500

    added rgb hovering effect and added a reset button to reset the grid to 16x16

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
 const button = document.getElementById("resizeBtn");
-
 const reset = document.getElementById("reset");
+
+function randomRGB (){
+    const r = Math.floor(Math.random() * 265); // random ineger from 0 to 256
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
 // create the grid canvas
 function createGrid(size){
@@ -23,10 +29,11 @@ function createGrid(size){
     // iterate through each square and add a mouse listener to "hover"
     squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
-            // Set background color to black
-            square.style.backgroundColor = "black";
+            // Set background color to random RGB
+            const randomColor = randomRGB();
+            square.style.backgroundColor = randomColor;
 
-            // Add increasing opacity everytime you hover over a square.
+            // Add increasing opacity everytime you hover over a square by 0.1.
             let currentOpacity = parseFloat(square.style.opacity);
             if(currentOpacity < 1){
                 square.style.opacity = (currentOpacity + 0.1).toString();
@@ -50,7 +57,7 @@ button.addEventListener("click", () => {
     }
 });
 
-// makes the graph back to a 16 x16;
+// makes the graph back to a 16x16;
 reset.addEventListener("click", () => {
     createGrid(16);
 });
